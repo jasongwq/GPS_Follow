01ba680a901ba5cc531dc5e927be4962b30d7e72
jasongwq <jasongwq@gmail.com>
2014-12-30 20:35:50 +0800

    first

---
+++ USER/TASK/tasks.h
@@ -0,0 +1,10 @@
+extern int task_bmp085(void);
+extern int task_ultrasonic(void);
+extern int task_pwm_ex(void);
+extern int task_led(void);
+extern int task_hmc5883l(void);
+extern int task_cap_rc(void);
+extern int loop_fast(void);//500hz
+extern int task_6050(void);
+extern int task_fast(void);//500hz
+
---
+++ USER/main.cpp
@@ -0,0 +1,56 @@
+/***SYS***/
+#include "sys.h"
+#include "gpio.h"
+#include <string.h>
+#include "usr_usart.h"
+#include "minos_delay.h"
+#include "sys_os.h"
+#include "gpio.h"
+#include "stm32_config.h"
+#include "G32_timer.h"
+/***C***/
+#include "string.h"
+/***Other***/
+#include "./TASK/tasks.h"
+
+void SYS_INIT(void)
+{
+    /***延时初始化***/
+    delay_init();
+    /***中断初始化***/
+    NVIC_Configuration();
+    //uart_init (115200);
+    //uart_init (115200);
+
+    uart3_init(115200);
+
+    // Sys_Printf(USART1, "\r\nUSART1 ok");
+    // Sys_Printf(USART2, "\r\nUSART2 ok");
+    // Sys_Printf(USART3, "\r\nUSART3 ok");
+
+    Sys_Printf(Printf_USART, (char *)"\r\nPrintf_USART ok");
+
+    delay_ms(500);
+}
+extern int task_6050(void);
+
+int main(void)
+{
+    SYS_INIT();
+    /***总循环***/
+    while (1)
+    {
+        RunTaskA(task_6050, 0);
+        RunTaskA(task_fast, 1);
+#if VERSIONS ==1
+#elif VERSIONS ==2
+        RunTaskA(task_cap_rc, 2);
+#endif
+        RunTaskA(task_ultrasonic, 3);
+				//RunTaskA(task_bmp085, 4);
+        //        RunTaskA(task_hmc5883l, 6);
+        
+        RunTaskA(task_pwm_ex, 5);
+        RunTaskA(task_led, 6);
+    }
+}
